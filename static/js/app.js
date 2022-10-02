738e43334be811fdf4df5c91b43cabea3f217781
FirstName LastName <maimyang@live.com>
2022-10-01 23:37:51 -0500

    added a trace object x and y ticks.

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,40 @@ console.log('This is app.js');
 
 // This code was largely inlfluenced by Instructor Dom
 
+// Define a global variable to hold the URLlet url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
 // Create the function stubs
 
 function DrawBargraph(sampleId)
 {
     console.log(`DrawBargraph(${sampleId})`);
+
+    d3.json(url).then(data => {
+        console.log(data);
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`);
+   
+
+        // Create a trace object
+        let barData = {
+            x: sample_values.slice(0, 10),
+            y: 
+        }
+
+        // Put the trace object into an array
+        // create a layout object
+        // Call the Plotly function
+
+    });
 
 }
 
@@ -43,7 +72,6 @@ function InitDashboard()
     //This is to get a handle to the dropdown
     let selector = d3.select("#selDataset");
 
-    let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
     // This is the code that I want to have run with with my data
     d3.json(url).then( data => {
